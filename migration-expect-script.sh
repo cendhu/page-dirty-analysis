518a709e5d1d46a8933ae6e1561f90045dec7574
Anand Soni <anand.92.soni@gmail.com>
2014-08-27 21:21:26 +0530

    Changes in arguments structure

--- migration-expect-script.sh
+++ migration-expect-script.sh
@@ -2,10 +2,10 @@
 set timeout 600
 set addr [lindex $argv 0]
 set size [lindex $argv 1]
-set fname [lindex $argv 2]
+set fname [lindex $argv 3]
 #set hcpu [lindex $argv 3]
 #set index [lindex $argv 4]
-set src [lindex $argv 3]
+set src [lindex $argv 2]
 set tport [lindex $argv 4]
 
 spawn telnet $src $tport
