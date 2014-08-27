efac5d903b894d305b0af282eff2d97b74d0dad2
Anand Soni <anand.92.soni@gmail.com>
2014-08-27 19:45:44 +0530

    Expect script to repeatedly call pmemsave

--- migration-expect-script.sh
+++ migration-expect-script.sh
@@ -3,13 +3,14 @@ set timeout 600
 set addr [lindex $argv 0]
 set size [lindex $argv 1]
 set fname [lindex $argv 2]
-set hcpu [lindex $argv 3]
-set index [lindex $argv 4]
-set src [lindex $argv 5]
-set tport [lindex $argv 6]
+#set hcpu [lindex $argv 3]
+#set index [lindex $argv 4]
+set src [lindex $argv 3]
+set tport [lindex $argv 4]
 
 spawn telnet $src $tport
-
-expect "(qemu)"
-send "memsave $addr $size $fname\r"
-expect "(qemu)"
+while {1} {
+  expect "(qemu)"
+  send "pmemsave $addr $size $fname\r"
+  sleep 2
+}
--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-watch -n 1 migration-expect-script.sh
