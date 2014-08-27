e933e2f4d5704ec60b00a510fefcee37d62fc70a
Anand Soni <anand.92.soni@gmail.com>
2014-08-27 22:51:26 +0530

    Generating dump file for each call

--- migration-expect-script.sh
+++ migration-expect-script.sh
@@ -2,15 +2,17 @@
 set timeout 600
 set addr [lindex $argv 0]
 set size [lindex $argv 1]
-set fname [lindex $argv 3]
-#set hcpu [lindex $argv 3]
-#set index [lindex $argv 4]
 set src [lindex $argv 2]
-set tport [lindex $argv 4]
+set tport [lindex $argv 3]
 
 spawn telnet $src $tport
+set c 0
 while {1} {
+  set filename "\"/tmp/file"
+  append filename $c
+  append filename ".dump\""
   expect "(qemu)"
-  send "pmemsave $addr $size $fname\r"
+  send "pmemsave $addr $size $filename\r"
   sleep 2
+  incr c 2
 }
